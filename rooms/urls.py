1cc724e2f40a9e59f4e28cf7243237dc62333be7
Guilherme Luis <guile.hm@hotmail.com>
2018-09-14 17:19:00 -0300

    Install DRF and create api app

--- rooms/urls.py
+++ rooms/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
 
 from rooms import settings
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
