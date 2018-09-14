428fa1041b8296f21b25fc34326ff2dfae92147b
Guilherme Luis <guile.hm@hotmail.com>
2018-09-14 16:50:37 -0300

    Create core app, staticfiles, url and view files

--- rooms/settings.py
+++ rooms/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'debug_toolbar',
+    'core',
 ]
 
 MIDDLEWARE = [
--- rooms/urls.py
+++ rooms/urls.py
@@ -20,6 +20,7 @@ from rooms import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
 ]
 
 if settings.DEBUG:
