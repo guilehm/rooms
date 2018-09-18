a8da30dd3d255d910b00bc9457b59fff87f5176f
Guilherme Luis <guile.hm@hotmail.com>
2018-09-18 01:02:31 -0300

    Create meeting change form

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from core.models import Room
+from core.models import Meeting, Room
 
 
 class RoomChangeForm(forms.ModelForm):
@@ -11,4 +11,20 @@ class RoomChangeForm(forms.ModelForm):
             'description',
             'active',
             'color',
+        )
+
+
+class MeetingChangeForm(forms.ModelForm):
+    date = forms.DateField(input_formats=['%d/%m/%Y'])
+
+    class Meta:
+        model = Meeting
+        fields = (
+            'room',
+            'name',
+            'description',
+            'status',
+            'date',
+            'start',
+            'end',
         )
