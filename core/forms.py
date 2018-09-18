08434ad43fc3cc684a8f75efb3201cb25bbd0767
Guilherme Luis <guile.hm@hotmail.com>
2018-09-17 23:19:03 -0300

    Create room change form

---
+++ core/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from core.models import Room
+
+
+class RoomChangeForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = (
+            'name',
+            'description',
+            'active',
+            'color',
+        )
