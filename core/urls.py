3dd279fa39ef6905eacc3ba05a8ed49da0b50735
Guilherme Luis <guile.hm@hotmail.com>
2018-09-17 23:44:52 -0300

    Create room add and change urls

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,8 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('room/list/', views.room_list, name='room-list'),
+    path('room/add/', views.room_add, name='room-add'),
+    path('room/<int:room_id>/change/', views.room_change, name='room-change'),
     path('meeting/list/', views.meeting_list, name='meeting-list'),
     path('meeting/calendar/', views.meeting_calendar, name='meeting-calendar'),
 ]
